5353dc463925fc4a4883a83a05c52914952efaa6
Vargentum <vargentum@gmail.com>
2016-02-29 16:37:19 +0200

    kata 4 text-justify (not ended)

---
+++ src/_modules/codewars/4/string-justify/tests/StringJustify.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import StringJustify from '../StringJustify';
+
+describe('StringJustify function', function() {
+
+
+  it('Should run a few assertions', () => {
+    expect(StringJustify).toBeDefined();
+  });
+
+  
+
+});
