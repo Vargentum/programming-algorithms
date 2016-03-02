a516b337e865affe7a2ba1887402735d94da2c9e
Vargentum <vargentum@gmail.com>
2016-03-02 18:18:29 +0200

    02 palindrome

--- src/_modules/codecamp/algo0/algo0.js
+++ src/_modules/codecamp/algo0/algo0.js
@@ -2,12 +2,24 @@
 
 export default function Algo0 () {
 
-  let factorialize = function factorialize (num) {
+  const reverse = function reverse(str) {
+    return str
+      .split('')
+      .reverse()
+      .join('')
+  }
+
+  const factorialize = function factorialize (num) {
     if (!num) return 1
     else return num * factorialize(num - 1)
   }
 
+  const palindrome = function palindrome (str) {
+    let simplify = str => str.replace(/[^0-9a-z]/gi, '').toLowerCase()
+    return simplify(str) === simplify(reverse(str))
+  }
 
-  return {factorialize}
+
+  return {reverse, factorialize, palindrome}
 
 }
--- src/_modules/codecamp/algo0/tests/algo0.test.js
+++ src/_modules/codecamp/algo0/tests/algo0.test.js
@@ -4,17 +4,34 @@ import Algo0 from '../algo0';
 
 describe('Algo0 View', function() {
 
-  let {factorialize} = Algo0()
+  let {factorialize, palindrome} = Algo0()
 
   it('Should run a few assertions', () => {
     expect(Algo0()).toBeDefined();
   });
 
 
-  it('Should factorialize number', () => {
-    expect(factorialize(0)).toBe(1)
-    expect(factorialize(3)).toBe(1*2*3)
-    expect(factorialize(5)).toBe(1*2*3*4*5)
-  });
+  describe('factorialize', () => {
+    it('Should factorialize number', () => {
+      expect(factorialize(0)).toBe(1)
+      expect(factorialize(3)).toBe(1*2*3)
+      expect(factorialize(5)).toBe(1*2*3*4*5)
+    });
+  })
+
+
+  describe('Palindrome', () => {
+    it('Should check if string in reverse order equal to origin string', () => {
+      expect(palindrome('Ele’ele')).toBe(true)
+      expect(palindrome('1Ele’ele')).toBe(false)
+      expect(palindrome('almostomla')).toBe(false)
+    });
+    it('Should also check phrases. Punctuation, spaces and case should be ignored', () => {
+      expect(palindrome('Doc, note, I Dissent. A fast never prevents a fatness. I diet on cod.')).toBe(true)
+      expect(palindrome('POW, ami! O’ Gad, ami! Go hang a salami, doc! Note; I dissent. A fast never prevents a fatness. I diet on cod. I’m a lasagna hog. I’m a dago! I’m a wop!')).toBe(true)
+      expect(palindrome('1Doc, note, I Dissent. A fast never prevents a fatness. I diet on cod.')).toBe(false)
+      expect(palindrome('1POW, ami! O’ Gad, ami! Go hang a salami, doc! Note; I dissent. A fast never prevents a fatness. I diet on cod. I’m a lasagna hog. I’m a dago! I’m a wop!')).toBe(false)
+    })
+  })
 
 });
