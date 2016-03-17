f01db4cdae991b4728734465faab8d0a7ffcc5ae
Vargentum <vargentum@gmail.com>
2016-03-17 16:51:29 +0200

    sym difference

--- src/_modules/codecamp/frontEnd/advanced.js
+++ src/_modules/codecamp/frontEnd/advanced.js
@@ -1,14 +1,29 @@
 'use strict';
 
-
 export default (() => {
+  
   const telephoneCheck = function telephoneChck(str) {
     const validUSNumPtrn = /^1?\s?((\(\d{3}\))|(\d{3}))\s?\-?\d{3}(\s|\-)?\d{4}$/
     return validUSNumPtrn.test(str)
   }
 
+  const sym = function sym(...arrs) {
+    let rmRepeated = (a) => a.reduce((p,n) => {
+      return p.indexOf(n) === -1 ? p.concat([n]) : p
+    },[])
+    let getSym = (a,b) => {
+      return [].concat(
+        a.filter(x => b.indexOf(x) === -1),
+        b.filter(x => a.indexOf(x) === -1)
+      )
+    }
+    return arrs
+      .reduce((p,n) => getSym(p, rmRepeated(n)), [])
+      .sort((a,b) => a - b)
+  } 
 
   return {
     telephoneCheck
+    ,sym
   }
 })()
--- src/_modules/codecamp/frontEnd/tests/advanced.test.js
+++ src/_modules/codecamp/frontEnd/tests/advanced.test.js
@@ -2,6 +2,7 @@
 
 import {
   telephoneCheck
+  ,sym
 
 } from '../advanced'
 
@@ -36,6 +37,15 @@ describe('Advanced CodeCamp JS Algorhitms', () => {
     })
   })
 
+
+  describe('sym', () => {
+    it(`Should return the symmetric difference of providen arrays, sorted in ascending order`, () => {
+      expect(sym([1, 2, 3], [5, 2, 1, 4])).toEqual([3,4,5])
+      expect(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])).toEqual([2, 3, 4, 6, 7])
+      expect(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])).toEqual([1, 2, 4, 5, 6, 7, 8, 9])
+      expect(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])).toEqual([1, 4, 5])
+    })
+  })
 
   // describe('', () => {
   //   it(`Should `, () => {
